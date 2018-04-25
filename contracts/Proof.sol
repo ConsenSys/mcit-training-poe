ef0048d05972416121e5ac2559db300b620ad2f1
samaphp <samaphp@gmail.com>
2018-04-25 13:08:37 +0300

    Fix some lines to meet latest Solidity best practice.

--- contracts/Proof.sol
+++ contracts/Proof.sol
@@ -11,20 +11,20 @@ contract Proof
     mapping (string => FileDetails) files;
     event logFileAddedStatus(bool status, uint timestamp, string owner, string fileHash);
 
-    function set(string owner, string fileHash)
+    function set(string owner, string fileHash) public
     {
         if(files[fileHash].timestamp == 0)
         {
             files[fileHash] = FileDetails(block.timestamp, owner);
-            logFileAddedStatus(true, block.timestamp, owner, fileHash);
+            emit logFileAddedStatus(true, block.timestamp, owner, fileHash);
         }
         else
         {
-            logFileAddedStatus(false, block.timestamp, owner, fileHash);
+            emit logFileAddedStatus(false, block.timestamp, owner, fileHash);
         }
     }
 
-    function get(string fileHash) returns (uint timestamp, string owner)
+    function get(string fileHash) public view returns (uint timestamp, string owner)
     {
         return (files[fileHash].timestamp, files[fileHash].owner);
     }
